f6d94e37679ad6652cb243ffd01e1127d38841dc
faiz.rehman1 <faizkhanabid@yahoo.com>
2023-06-09 01:06:02 +0500

    different datatypes

--- Class 1/Class1.dart
+++ Class 1/Class1.dart
@@ -2,4 +2,18 @@ void main(){
 
     print("Hello World");
 
+    num number=12;
+
+    print(number); 
+
+    var abc;
+    int aa = 10;
+    print(aa);
+
+    abc = "Helo";
+    abc = 10;
+
+    print(abc);
+
+
 }
