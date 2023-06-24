65ebc30d53a3633ded445095415741e784446fac
faiz.rehman1 <faizkhanabid@yahoo.com>
2023-06-24 17:20:19 +0500

    Add Classes work

---
+++ Class 5/Class_5.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Indexed List
+  var studenNames = ["Faiz", "Bilal"];
+
+  //  Map List
+  var studentRecord = {"name": "Saqib", "rollNum": "141042"};
+
+  List studentRecords = [
+    {
+      "name": "Faiz",
+      "rollNum": [12, 34, 7645]
+    },
+    {"name": "Bilal", "rollNum": 141042}
+  ];
+  // print(studentRecords[0]['rollNum'][1]);
+
+  var fruit = {1: "Apple", 2: "Mango"};
+  fruit[3] = "Banana";
+  fruit.putIfAbsent(3, () => "TEST");
+
+  // print(fruit);
+}
